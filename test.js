4f8ff7312bcf304096ba65230110e6e4937ded5d
Lupo Montero <lupo@e-noise.com>
2014-04-02 21:11:58 +0200

    Handle errors a bit more gracefully and added more tests #1

--- test.js
+++ test.js
@@ -4,7 +4,48 @@ var options = {
   secret: process.env.AWSSECRETACCESSKEY
 };
 
-exports.test1 = function (t) {
+exports.testUrlInfoNoUrl = function (t) {
+  var client = awis(options);
+  client({
+    'Action': 'UrlInfo',
+    'Url': '',
+    'ResponseGroup': 'Related,TrafficData,ContentData'
+  }, function (err, res) {
+    t.ok(err instanceof Error);
+    t.ok(/RequiredParameterMissing/.test(err.message));
+    t.ok(!res);
+    t.done();
+  });
+};
+
+exports.testUrlInfoBadDomain = function (t) {
+  var client = awis(options);
+  client({
+    'Action': 'UrlInfo',
+    'Url': 'github.comm&^%$&*',
+    'ResponseGroup': 'Related,TrafficData,ContentData'
+  }, function (err, res) {
+    t.ok(err instanceof Error);
+    t.ok(/SignatureDoesNotMatch/.test(err.message));
+    t.ok(!res);
+    t.done();
+  });
+};
+
+exports.testUrlInfoUrlNotFound = function (t) {
+  var client = awis(options);
+  client({
+    'Action': 'UrlInfo',
+    'Url': 'some-non-existing-domain.sometld',
+    'ResponseGroup': 'ContentData'
+  }, function (err, res) {
+    t.ok(!err);
+    t.equal(res.contentData.dataUrl, '404');
+    t.done();
+  });
+};
+
+exports.testUrlInfoAll = function (t) {
   var client = awis(options);
   client({
     'Action': 'UrlInfo',
@@ -18,6 +59,52 @@ exports.test1 = function (t) {
     t.equal(res.trafficData.dataUrl, 'github.com');
     t.ok(res.related);
     t.equal(res.related.dataUrl, 'github.com');
+    t.done();
+  });
+};
+
+exports.testUrlInfoAll2 = function (t) {
+  var client = awis(options);
+  client({
+    'Action': 'UrlInfo',
+    'Url': 'monono.org',
+    'ResponseGroup': 'Related,TrafficData,ContentData'
+  }, function (err, res) {
+    t.ok(!err);
+    t.ok(res.contentData);
+    t.equal(res.contentData.dataUrl, 'monono.org');
+    t.ok(res.trafficData);
+    t.equal(res.trafficData.dataUrl, 'monono.org');
+    t.ok(res.related);
+    t.equal(res.related.dataUrl, 'monono.org');
+    t.done();
+  });
+};
+
+exports.testUrlInfoRelated = function (t) {
+  var client = awis(options);
+  client({
+    'Action': 'UrlInfo',
+    'Url': 'monono.org',
+    'ResponseGroup': 'Related'
+  }, function (err, res) {
+    t.ok(!err);
+    t.ok(res.related);
+    t.equal(res.related.dataUrl.replace(/\/$/, ''), 'monono.org');
+    t.done();
+  });
+};
+
+exports.testUrlInfoTrafficData = function (t) {
+  var client = awis(options);
+  client({
+    'Action': 'UrlInfo',
+    'Url': 'weibo.com',
+    'ResponseGroup': 'TrafficData'
+  }, function (err, res) {
+    t.ok(!err);
+    t.ok(res.trafficData);
+    t.equal(res.trafficData.dataUrl.replace(/\/$/, ''), 'weibo.com');
     t.done();
   });
 };
