d9dcf1cbabd68597cc6e218b91fa7cc7148a2de8
Lupo Montero <lupomontero@gmail.com>
2015-12-14 03:05:53 +0000

    house keeping...

--- index.js
+++ index.js
@@ -1,83 +1,121 @@
-var util = require('util');
-var crypto = require('crypto');
-var _ = require('lodash');
-var xml2js = require('xml2js');
-var apiDomain = 'awis.amazonaws.com';
-var request = require('request').defaults({ url: 'http://' + apiDomain });
+'use strict';
 
-function formatTagName(name) {
+const Util = require('util');
+const Crypto = require('crypto');
+const _ = require('lodash');
+const Xml2js = require('xml2js');
+const Request = require('request');
+
+
+const internals = {
+  apiDomain: 'awis.amazonaws.com'
+};
+
+
+
+internals.formatTagName = function (name) {
+
   // Remove 'aws:' prefix.
   name = name.replace(/^aws:/, '');
   // Make first char lower case.
   return name.charAt(0).toLowerCase() + name.slice(1);
-}
+};
 
-function walk(data) {
+
+internals.walk = function (data) {
+
   if (_.isArray(data)) {
     if (data.length > 1) {
-      return _.map(data, function (item) { return walk(item); });
-    } else if (data.length === 1) {
-      return walk(data[0]);
+      return _.map(data, (item) => {
+
+        return internals.walk(item);
+      });
     }
-  } else if (_.isObject(data)) {
-    return _.reduce(data, function (memo, v, k) {
-      memo[formatTagName(k)] = walk(v);
+    else if (data.length === 1) {
+      return internals.walk(data[0]);
+    }
+  }
+  else if (_.isObject(data)) {
+    return _.reduce(data, (memo, v, k) => {
+
+      memo[internals.formatTagName(k)] = internals.walk(v);
       return memo;
     }, {});
-  } else {
+  }
+  else {
     return data;
   }
-}
+};
 
-function parse(xml, req, cb) {
-  var action = req.Action;
-  var responseKey = 'aws:' + action + 'Response';
-  var actionResultKey = 'aws:' + action + 'Result';
 
-  var validateResponse = function (data, json) {
+internals.parse = function (xml, req, cb) {
+
+  const action = req.Action;
+  const responseKey = 'aws:' + action + 'Response';
+  const actionResultKey = 'aws:' + action + 'Result';
+
+  const validateResponse = function (data, json) {
+
     if (!_.isArray(data['aws:ResponseStatus'])) {
       throw new Error('XML response missing aws:ResponseStatus!\n' + json);
     }
+
     if (!_.isArray(data['aws:ResponseStatus'][0]['aws:StatusCode'])) {
       throw new Error('XML response missing aws:StatusCode!\n' + json);
     }
-    var statusCode = data['aws:ResponseStatus'][0]['aws:StatusCode'][0];
+
+    const statusCode = data['aws:ResponseStatus'][0]['aws:StatusCode'][0];
     if (statusCode !== 'Success') {
       throw new Error(statusCode + '\n' + json);
     }
+
     if (!_.isArray(data[actionResultKey]) || !data[actionResultKey].length) {
       throw new Error('XML response missing ' + actionResultKey + '!\n' + json);
     }
+
     data = data[actionResultKey][0]['aws:Alexa'][0];
-    return walk(data);
+    return internals.walk(data);
   };
 
-  xml2js.parseString(xml, { ignoreAttrs: true, trim: true }, function (err, data) {
-    if (err) { return cb(err); }
+
+  Xml2js.parseString(xml, { ignoreAttrs: true, trim: true }, (err, data) => {
+
+    if (err) {
+      return cb(err);
+    }
+
     // json string representation of response data (for debugging).
-    var json = JSON.stringify(data, null, '  ');
+    const json = JSON.stringify(data, null, '  ');
+
     // We need to know the request action in order to know the tag names.
     if (data.Response && _.isArray(data.Response.Errors)) {
-      return cb(new Error(data.Response.Errors.reduce(function (memo, error) {
-        if (memo) { memo += '\n'; }
-        return memo += error.Error[0].Code[0] + ': ' + error.Error[0].Message[0];
+      return cb(new Error(data.Response.Errors.reduce((memo, error) => {
+
+        if (memo) {
+          memo += '\n';
+        }
+        memo += error.Error[0].Code[0] + ': ' + error.Error[0].Message[0];
+        return memo;
       }, '')));
     }
+
     if (!data[responseKey] || !_.isArray(data[responseKey]['aws:Response'])) {
       return cb(new Error('XML response missing aws:Response!\n' + json));
     }
+
     // Discard top level nodes. We are only interested on whats inside
     // `aws:Alexa`.
     data = data[responseKey]['aws:Response'];
 
     // Batch requests support
-    var all = [];
+    let all = [];
     try {
-      for (var i = 0, l = data.length; i < l; i++) {
+      for (let i = 0; i < data.length; ++i) {
         // Require all responses to be valid
         all.push(validateResponse(data[i], json));
       }
-    } catch (e) {
+    }
+    catch (e) {
       return cb(e);
     }
 
@@ -88,9 +126,11 @@ function parse(xml, req, cb) {
 
     cb(null, all);
   });
-}
+};
 
-function query(req, options) {
+
+internals.query = function (req, options) {
+
   req.SignatureMethod = 'HmacSHA256';
   req.SignatureVersion = 2;
   req.AWSAccessKeyId = options.key;
@@ -99,29 +139,42 @@ function query(req, options) {
   // Sign...
   // Request keys must be sorted with natural byte ordering.
   // http://docs.aws.amazon.com/AlexaWebInfoService/latest/index.html?CalculatingSignatures.html
-  var keys = Object.keys(req).sort();
-  var q = keys.reduce(function (memo, k) {
-    if (memo) { memo += '&'; }
+  const keys = Object.keys(req).sort();
+  const q = keys.reduce((memo, k) => {
+
+    if (memo) {
+      memo += '&';
+    }
     // Manually replace single quotes with `%27` as `encodeURIComponent` doesnt
     // seem to encode them, and things break:
     // https://github.com/wrangr/awis/issues/3
-    var value = encodeURIComponent(req[k]).replace(/'/g, '%27');
+    const value = encodeURIComponent(req[k]).replace(/'/g, '%27');
     return memo + encodeURIComponent(k) + '=' + value;
   }, '');
-  var tmpl = 'GET\n%s\n/\n%s';
-  var stringToSign = util.format(tmpl, apiDomain, q);
-  var signature = crypto.createHmac('SHA256', options.secret);
+  const tmpl = 'GET\n%s\n/\n%s';
+  const stringToSign = Util.format(tmpl, internals.apiDomain, q);
+  const signature = Crypto.createHmac('SHA256', options.secret);
   signature.update(stringToSign);
   req.Signature = signature.digest('base64');
 
   return req;
-}
+};
+
 
 module.exports = function (options) {
+
   return function (req, cb) {
-    request({ qs: query(req, options) }, function (err, res) {
-      if (err) { return cb(err); }
-      parse(res.body, req, cb);
+
+    Request({
+      url: 'http://' + internals.apiDomain,
+      qs: internals.query(req, options)
+    }, (err, res) => {
+
+      if (err) {
+        return cb(err);
+      }
+
+      internals.parse(res.body, req, cb);
     });
   };
 };
--- test/awis.spec.js
+++ test/awis.spec.js
@@ -1,164 +1,192 @@
-var assert = require('assert');
-var awis = require('../');
-var options = {
+'use strict';
+
+
+const Assert = require('assert');
+const Awis = require('../');
+
+
+const options = {
   key: process.env.AWSACCESSKEYID,
   secret: process.env.AWSSECRETACCESSKEY
 };
 
-describe('awis', function () {
 
-  it('should fail when no url is specified', function (done) {
-    var client = awis(options);
-    client({
+describe('Awis', () => {
+
+
+  it('should fail when no url is specified', (done) => {
+
+    Awis(options)({
       'Action': 'UrlInfo',
       'Url': '',
       'ResponseGroup': 'Related,TrafficData,ContentData'
-    }, function (err, res) {
-      assert.ok(err instanceof Error);
-      assert.ok(/RequiredParameterMissing/.test(err.message));
-      assert.ok(!res);
+    }, (err, res) => {
+
+      Assert.ok(err instanceof Error);
+      Assert.ok(/RequiredParameterMissing/.test(err.message));
+      Assert.ok(!res);
       done();
     });
   });
 
-  it('should fail when bad domain', function (done) {
-    var client = awis(options);
-    client({
+
+  it('should fail when bad domain', (done) => {
+
+    Awis(options)({
       'Action': 'UrlInfo',
       'Url': 'github.comm&^%$&*',
       'ResponseGroup': 'Related,TrafficData,ContentData'
-    }, function (err, res) {
-      assert.ok(err instanceof Error);
-      assert.ok(/SignatureDoesNotMatch/.test(err.message));
-      assert.ok(!res);
+    }, (err, res) => {
+
+      Assert.ok(err instanceof Error);
+      Assert.ok(/SignatureDoesNotMatch/.test(err.message));
+      Assert.ok(!res);
       done();
     });
   });
 
-  it('should result in 404 when domain doesnt resolve', function (done) {
-    var client = awis(options);
-    client({
+
+  it('should result in 404 when domain doesnt resolve', (done) => {
+
+    Awis(options)({
       'Action': 'UrlInfo',
       'Url': 'some-non-existing-domain.sometld',
       'ResponseGroup': 'ContentData'
-    }, function (err, res) {
-      assert.ok(!err);
-      assert.equal(res.contentData.dataUrl, '404');
+    }, (err, res) => {
+
+      Assert.ok(!err);
+      Assert.equal(res.contentData.dataUrl, '404');
       done();
     });
   });
 
-  it('should get all response groups', function (done) {
-    var client = awis(options);
-    client({
+
+  it('should get all response groups', (done) => {
+
+    Awis(options)({
       'Action': 'UrlInfo',
       'Url': 'github.com',
       'ResponseGroup': 'Related,TrafficData,ContentData'
-    }, function (err, res) {
-      assert.ok(!err);
-      assert.ok(res.contentData);
-      assert.equal(res.contentData.dataUrl, 'github.com');
-      assert.ok(res.trafficData);
-      assert.equal(res.trafficData.dataUrl, 'github.com');
-      assert.ok(res.related);
-      assert.equal(res.related.dataUrl, 'github.com');
+    }, (err, res) => {
+
+      Assert.ok(!err);
+      Assert.ok(res.contentData);
+      Assert.equal(res.contentData.dataUrl, 'github.com');
+      Assert.ok(res.trafficData);
+      Assert.equal(res.trafficData.dataUrl, 'github.com');
+      Assert.ok(res.related);
+      Assert.equal(res.related.dataUrl, 'github.com');
       done();
     });
   });
 
-  it('should get all response groups (again)', function (done) {
-    var client = awis(options);
-    client({
+
+  it('should get all response groups (again)', (done) => {
+
+    Awis(options)({
       'Action': 'UrlInfo',
       'Url': 'monono.org',
       'ResponseGroup': 'Related,TrafficData,ContentData'
-    }, function (err, res) {
-      assert.ok(!err);
-      assert.ok(res.contentData);
-      assert.equal(res.contentData.dataUrl, 'monono.org');
-      assert.ok(res.trafficData);
-      assert.equal(res.trafficData.dataUrl, 'monono.org');
-      assert.ok(res.related);
-      assert.equal(res.related.dataUrl, 'monono.org');
+    }, (err, res) => {
+
+      Assert.ok(!err);
+      Assert.ok(res.contentData);
+      Assert.equal(res.contentData.dataUrl, 'monono.org');
+      Assert.ok(res.trafficData);
+      Assert.equal(res.trafficData.dataUrl, 'monono.org');
+      Assert.ok(res.related);
+      Assert.equal(res.related.dataUrl, 'monono.org');
       done();
     });
   });
 
-  it('should get related sites', function (done) {
-    var client = awis(options);
-    client({
+
+  it('should get related sites', (done) => {
+
+    Awis(options)({
       'Action': 'UrlInfo',
       'Url': 'monono.org',
       'ResponseGroup': 'Related'
-    }, function (err, res) {
-      assert.ok(!err);
-      assert.ok(res.related);
-      assert.equal(res.related.dataUrl.replace(/\/$/, ''), 'monono.org');
+    }, (err, res) => {
+
+      Assert.ok(!err);
+      Assert.ok(res.related);
+      Assert.equal(res.related.dataUrl.replace(/\/$/, ''), 'monono.org');
       done();
     });
   });
 
-  it('should get traffic data', function (done) {
-    var client = awis(options);
-    client({
+
+  it('should get traffic data', (done) => {
+
+    Awis(options)({
       'Action': 'UrlInfo',
       'Url': 'weibo.com',
       'ResponseGroup': 'TrafficData'
-    }, function (err, res) {
-      assert.ok(!err);
-      assert.ok(res.trafficData);
-      assert.equal(res.trafficData.dataUrl.replace(/\/$/, ''), 'weibo.com');
+    }, (err, res) => {
+
+      Assert.ok(!err);
+      Assert.ok(res.trafficData);
+      Assert.equal(res.trafficData.dataUrl.replace(/\/$/, ''), 'weibo.com');
       done();
     });
   });
 
-  it('should get traffic history', function (done) {
-    awis(options)({
+
+  it('should get traffic history', (done) => {
+
+    Awis(options)({
       'Action': 'TrafficHistory',
       'Url': 'lupomontero.com',
       'ResponseGroup': 'History'
-    }, function (err, res) {
-      assert.ok(!err);
-      assert.ok(res.trafficHistory);
-      assert.ok(res.trafficHistory.range);
-      assert.equal(res.trafficHistory.site, 'lupomontero.com');
-      assert.ok(res.trafficHistory.start);
-      assert.ok(res.trafficHistory.historicalData);
-      assert.ok(res.trafficHistory.historicalData.data);
-      assert.ok(res.trafficHistory.historicalData.data.length);
-      res.trafficHistory.historicalData.data.forEach(function (item) {
-        assert.ok(item.date);
-        assert.ok(item.pageViews);
-        assert.ok(item.rank);
-        assert.ok(item.reach);
+    }, (err, res) => {
+
+      Assert.ok(!err);
+      Assert.ok(res.trafficHistory);
+      Assert.ok(res.trafficHistory.range);
+      Assert.equal(res.trafficHistory.site, 'lupomontero.com');
+      Assert.ok(res.trafficHistory.start);
+      Assert.ok(res.trafficHistory.historicalData);
+      Assert.ok(res.trafficHistory.historicalData.data);
+      Assert.ok(res.trafficHistory.historicalData.data.length);
+      res.trafficHistory.historicalData.data.forEach((item) => {
+
+        Assert.ok(item.date);
+        Assert.ok(item.pageViews);
+        Assert.ok(item.rank);
+        Assert.ok(item.reach);
       });
       done();
     });
   });
 
-  it('should get sites linking in', function (done) {
-    var client = awis(options);
-    client({
+
+  it('should get sites linking in', (done) => {
+
+    Awis(options)({
       'Action': 'SitesLinkingIn',
       'Url': 'yahoo.com',
       'ResponseGroup': 'SitesLinkingIn',
       'Count': 20
-    }, function (err, res) {
-      assert.ok(!err);
-      assert.ok(res.sitesLinkingIn);
-      assert.ok(res.sitesLinkingIn.site);
-      assert.ok(res.sitesLinkingIn.site.length);
-      res.sitesLinkingIn.site.forEach(function (site) {
-        assert.ok(site.title);
-        assert.ok(site.url);
+    }, (err, res) => {
+
+      Assert.ok(!err);
+      Assert.ok(res.sitesLinkingIn);
+      Assert.ok(res.sitesLinkingIn.site);
+      Assert.ok(res.sitesLinkingIn.site.length);
+      res.sitesLinkingIn.site.forEach((site) => {
+
+        Assert.ok(site.title);
+        Assert.ok(site.url);
       });
       done();
     });
   });
 
-  it('should get rank in batch', function (done) {
-    var client = awis(options);
-    client({
+
+  it('should get rank in batch', (done) => {
+
+    Awis(options)({
       Action: 'UrlInfo',
       'UrlInfo.Shared.ResponseGroup': 'Rank',
       'UrlInfo.1.Url': 'lupomontero.com',
@@ -166,35 +194,40 @@ describe('awis', function () {
       'UrlInfo.3.Url': 'weibo.com',
       'UrlInfo.4.Url': 'github.com',
       'UrlInfo.5.Url': 'monono.org'
-    }, function (err, res) {
-      assert.ok(!err);
-      assert.ok(res.length === 5);
-      res.forEach(function (response) {
-        assert.ok(response.trafficData.dataUrl);
+    }, (err, res) => {
+
+      Assert.ok(!err);
+      Assert.ok(res.length === 5);
+      res.forEach((response) => {
+
+        Assert.ok(response.trafficData.dataUrl);
       });
       done();
     });
   });
 
-  it('should allow apostrophe in path when doing CategoryBrowse', function (done) {
-    var client = awis(options);
-    client({
+
+  it('should allow apostrophe in path when doing CategoryBrowse', (done) => {
+
+    Awis(options)({
       Action: 'CategoryBrowse',
       ResponseGroup: 'Categories',
       Path: 'Top/Shopping/Clothing/Children\'s'
-    }, function (err, res) {
-      assert.ok(!err);
-      assert.ok(res.categoryBrowse.categories.category);
-      assert.equal(typeof res.categoryBrowse.categories.category.length, 'number');
-      assert.ok(res.categoryBrowse.categories.category.length > 0);
-      res.categoryBrowse.categories.category.forEach(function (item) {
-        assert.equal(typeof item.path, 'string');
-        assert.equal(typeof item.title, 'string');
+    }, (err, res) => {
+
+      Assert.ok(!err);
+      Assert.ok(res.categoryBrowse.categories.category);
+      Assert.equal(typeof res.categoryBrowse.categories.category.length, 'number');
+      Assert.ok(res.categoryBrowse.categories.category.length > 0);
+      res.categoryBrowse.categories.category.forEach((item) => {
+
+        Assert.equal(typeof item.path, 'string');
+        Assert.equal(typeof item.title, 'string');
       });
       done();
     });
   });
 
-});
 
+});
 
